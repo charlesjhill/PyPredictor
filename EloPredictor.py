8709b14774bf2d2bc2734e7e93580e053b2ec71b
Charles Hill <hill3cj@mail.uc.edu>
2018-11-18 18:12:58 -0500

    Added parameters for elo-to-fb_pts conversion factor, and a flat bias adjustment. Also moved some functionality to non-static variants. Added functions to regress the ELO ratings to the average and added more performance indicator functions

--- EloPredictor.py
+++ EloPredictor.py
@@ -1,8 +1,39 @@
 from math import log
+from Team import Team
 
 
 class EloPredictor:
-    hta_adjustment = 2.5
+    hfa_adjustment = 1.3  # prev. 5.8
+    elo2pts_conv = 30.0     # 23
+    bias_adjust = 7    # +7.94
+
+    def __init__(self, games):
+        self.games = games
+
+    def get_abs_error(self):
+        g_list = self.games
+        l = [abs(EloPredictor.get_exp_margin(g) - g.get_margin()) for g in g_list]
+        return sum(l) / len(l)
+
+    def get_mse(self):
+        g_list = self.games
+        l = [pow(EloPredictor.get_exp_margin(g) - g.get_margin(), 2) for g in g_list]
+        return sum(l) / len(l)
+
+    def get_bias(self):
+        g_list = self.games
+        l = [EloPredictor.get_exp_margin(g) - g.get_margin() for g in g_list]
+        return sum(l) / len(l)
+
+    def get_pct_games_right(self):
+        g_list = self.games
+        l = [1 for g in g_list if EloPredictor.get_exp_margin(g) > 0]
+        return sum(l) / len(g_list)
+
+    def get_performance_info(self):
+        print(f"Abs Err: {self.get_abs_error()}")
+        print(f"Bias: {self.get_bias()}")
+        print(f"Pct. Correct: {self.get_pct_games_right()}")
 
     @staticmethod
     def update_teams_elo(g):
@@ -21,13 +52,17 @@ class EloPredictor:
 
     @staticmethod
     def get_exp_margin(g):
-        exp_margin = (g.winner.ELO - g.loser.ELO) / 25
+        exp_margin = (g.winner.ELO - g.loser.ELO) / EloPredictor.elo2pts_conv
         if g.home_win:
-            exp_margin += EloPredictor.hta_adjustment
+            exp_margin += EloPredictor.hfa_adjustment
         else:
-            exp_margin -= EloPredictor.hta_adjustment
-        return exp_margin
+            exp_margin -= EloPredictor.hfa_adjustment
+        return exp_margin + EloPredictor.bias_adjust
 
     @staticmethod
     def get_exp_prob_winning(g):
         return 1 / (pow(10, ((g.loser.ELO - g.winner.ELO) / 400)) + 1)
+
+    @staticmethod
+    def regress_elo(t):
+        t.ELO = (t.ELO - Team.default_rating) / 3 + Team.default_rating
